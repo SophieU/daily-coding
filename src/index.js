97289cd8602b9fa0ee321a96e176b1dd1ede036b
SophieU <j.u.sophia@qq.com>
2019-09-02 10:49:41 +0800

    92

--- src/index.js
+++ src/index.js
@@ -7,4 +7,21 @@ function compare(val1,val2) {
     return val1 - val2;
 }
 var arr2 = arr.sort(compare)
-console.log(arr2)
+
+/**
+ * 冒泡排序法：相邻两元素比较，小的放前
+ */
+function popupSort(arr) {
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 0; j < arr.length; j++) { 
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+var arr3 = popupSort(arr)
+console.log(arr3)

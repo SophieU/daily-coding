3abe14ad1364f3f13cad56909c67e52cd16e0ca9
SophieU <j.u.sophia@qq.com>
2019-09-12 14:39:24 +0800

    912

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-setTimeout(()=>{
-    console.log(1);
-}, 0);
-new Promise((resolve)=>{
-    console.log(2);
-    for(var i = 1; i < 200; i++){
-            i = 198 && resolve();
-    }
-    console.log(3);
-}).then(()=>{
-    console.log(4);
-});console.log(5);
+var set1 = new Set([1,4,5,67])
+var map1 = new Map([1,2,3])

efa7ad16788977627216c001cb202ae58bb1d093
SophieU <j.u.sophia@qq.com>
2019-09-14 15:10:35 +0800

    914

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-var set1 = new Set([1,4,5,67])
-var map1 = new Map([{ 1: 1 }])
+class Parent {
+    constructor() {
+        this.name='tl'
+    }
+}
+class Child extends Parent{
+    constructor() {
+        this.age = 1;
+    }
+}
+var child = new Child()

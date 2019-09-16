fa206250b85f67322e2243cd3d545817b164dc86
WIN-9M659CUL90U\Administrator <j.u.sophia@qq.com>
2019-09-16 15:24:21 +0800

    916

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-asfasfs
+/** 
+ * 一、初始化阶段
+ *  componentWillMount
+ *  render
+ * componentDidMount
+ * 二、运行阶段
+ * componentWillReciveProps
+ * shouldComponentUpdate
+ * componentWillUpdate
+ * componentDidUpdate
+ * 三、销毁阶段
+ * componentWillUnmount
+ */

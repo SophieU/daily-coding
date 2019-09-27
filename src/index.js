195e8b0f74e98ecb1f8c74db006b6f78d012fe13
WIN-9M659CUL90U\Administrator <j.u.sophia@qq.com>
2019-09-27 12:08:51 +0800

    927

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ react 中的props和state的用法？
 react组件之间如何 通信？ props 属性函数 redux
 为什么虚拟DOM会提高性能？virtualDOM 使用JS对象来描述DOM节点，通过一层虚拟的DOM树与真实DOM树比较，
 得到差异化的节点再对应更新差异部分，减少了额外的DOM我操作，达到优化性能的目的
+react 生命周期函数 ？
+初始化期：
+    componentWillMount
+    render
+    componentDidMount
+运行期：
+    componentWillRecieveProps
+    shouldComponentUpdate
+    componentUpdated
+销毁期：
+    componentWillUmmount
+    componentDidUnmount

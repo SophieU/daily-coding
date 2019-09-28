c49dfc99d452f84f212ba2f77e90796cd42982aa
WIN-9M659CUL90U\Administrator <j.u.sophia@qq.com>
2019-09-28 12:12:17 +0800

    928

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ react 生命周期函数 ？
 运行期：
     componentWillRecieveProps
     shouldComponentUpdate
-    componentUpdated
+    componentWillUpdate
+    render
 销毁期：
     componentWillUmmount
-    componentDidUnmount
+
+react性能优化是哪个周期函数？ shouldComponentUpdate
+哪一步发起请求？componentDidMount
+React事件处理逻辑？react将将原生事件经过处理成为合成事件（解决了跨浏览器的兼容性问题，不同浏览器对事件绑定的处理不一致），且事件并不是绑定在特定DOM元素上，而是统一
+由顶层组件控制，这样在DOM更新时，可以不用担心事件的处理。

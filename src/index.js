787f98e17d5998fa167f0153687a4cb40d5cafa0
WIN-9M659CUL90U\Administrator <j.u.sophia@qq.com>
2019-11-05 23:07:36 +0800

    extend js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
-function myCall(context){
-    context = context || window
-    fn = this;
-    var arg = [...arguments].slice(1)
-    var res = context.fn(...args)
-    delete context.fn;
-    return res;
+function Person(name){
+  this.name = name
 }
-function myApply(context){
-    context = context || window
-    fn = this
-    var res ;
-    if(arguments[1]){
-        res = context.fn(...arguments[1])
-    }else {
-        res = context.fn()
-    }
-    delete context.fn;
-    return res
+function Child(name){
+  Person.call(this,name)
 }
-Function.prototype.myBind = function (context) {
-    if (typeof this !== 'function') {
-      throw new TypeError('Error')
-    }
-    var _this = this
-    var args = [...arguments].slice(1)
-    // 返回一个函数
-    return function F() {
-      // 因为返回了一个函数，我们可以 new F()，所以需要判断
-      if (this instanceof F) {
-        return new _this(...args, ...arguments)
-      }
-      return _this.apply(context, args.concat(...arguments))
-    }
-  }
-  function test(){
-      console.log(this.a)
-  }
+Child.prototype.say= function(){
+  console.log(111)
+}
+

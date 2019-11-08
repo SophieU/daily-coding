51ef9d54f4df451bbb2294ddff720b8068cd39c1
WIN-9M659CUL90U\Administrator <j.u.sophia@qq.com>
2019-11-08 23:55:47 +0800

    11-08

--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@
 实现继承的几种方式（7种）： 原型继承，构造函数继承，拷贝继承，实例继承，组合继承，寄生组合继承，Class继承
 */
 // 原型继承
-function Parent (name){
+// function Parent (name){
+//     this.name=name;
+// }
+// function Child(){
+// }
+// Child.prototype = new Parent()
+
+// 构造函数
+// function Parent(name){
+//     this.name=name;
+// }
+// function Child(name){
+//     Parent.call(this,name)
+//     this.age=12;
+// }
+
+// 实例继承
+function Parent(name){
     this.name=name;
 }
-function Child(){
+function Child(name){
+   var instance = new Parent(name)
+   instance.age=12;
+   return instance
 }
-Child.prototype = new Parent()

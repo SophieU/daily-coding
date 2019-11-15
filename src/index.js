9f7b7ef507e1ebaffd46f7c7ab47f9ae9d018f85
WIN-9M659CUL90U\Administrator <j.u.sophia@qq.com>
2019-11-15 14:27:43 +0800

    review

--- src/index.js
+++ src/index.js
@@ -1,40 +1,10 @@
-Function.prototype.myBind = function(context){
-    if(typeof this !=='function'){
-        return;
-    }
-    let args = Array.prototype.slice.call(arguments,1);
-    let fn = this;
-    return function F(){
-        let argsInner = Array.from(arguments)
-        // 检测new
-        var _this  = this instanceof F ? this : context;
-       fn.apply(_this,args.concat(argsInner))
-    }
-}
-Function.prototype.myBind2 = function (context) {
-    if (typeof this !== 'function') {
-      throw new TypeError('Error')
-    }
-    var _this = this
-    var args = [...arguments].slice(1)
-    // 返回一个函数
-    return function F() {
-      // 因为返回了一个函数，我们可以 new F()，所以需要判断
-      if (this instanceof F) {
-          console.log('---------')
-          console.log(this instanceof F)
-        return new _this(...args, ...arguments)
-      }
-      return _this.apply(context, args.concat(...arguments))
-    }
-  }
-
-  function test(){
-      console.log(this.name)
-  }
-  var obj = {
-      name:'helloU'
-  }
-  var test2 = test.myBind2(obj);
-  console.log(111)
-var xxx = new test2();
+/**
+ * 继承的几种实现方法：原型继承，构造函数继承，拷贝继承，实例继承，组合继承，寄生组合继承，ES6继承
+ * 原型继承 ： Child.prototype = new Parent()
+ * 构造函数继承： Parent.call(this)
+ * 拷贝继承： Child.prototype[key] = Parent[key]
+ * 实例继承：var instance = new Parent() return instance
+ * 组合继承：Child.prototype = new Parent() Parent.call(this)
+ * 寄生组合继承： Child.prototype = Object.create(Parent.prototype,{contractor:Child,enumberable:false,configurable:true,writable:true})
+ * 
+ */

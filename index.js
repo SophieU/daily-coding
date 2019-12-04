573ca4bc7077ced817395e929122ea7d76071c9b
SophieU <j.u.sophia@qq.com>
2019-12-04 19:42:39 +0800

    total

--- index.js
+++ index.js
@@ -1,44 +1,11 @@
-Function.prototype.myCall = function (context) {
-    if (typeof this !== 'function') {
-        throw new Error('not a function')
-    }
-    let args = arguments.slice(1);
-    context.fn = this;
-    var res = context.fn(...args)
-    delete context.fn;
-    return res;
-}
-Function.prototype.myApply = function (context) {
-    if (typeof this !== 'function') {
-        throw new Error('not a function')
-    }
-    let args = arguments.slice(1);
-    context.fn = this;
-    var res;
-    if (!arguments[1]) {
-        res = context.fn();
-    } else {
-        res = context.fn(...arguments[1])
-    }
-    return res;
-}
-Function.prototype.myBind = function (context) {
-    if (typeof this !== 'function') {
-        throw new Error('not a function')
-    }
-    context = context || window;
-    let argsInner = Array.prototype.slice.call(arguments, 1);
-    const self = this;
-    const nop = function () { }
-    const bound = function () {
-        return self.apply(
-            this instanceof nop ? this : context,
-            argsInner.concat(Array.from(arguments))
-        )
-    }
-    if (this.prototype) {
-        nop.prototype = this.prototype;
-    }
-    bound.prototype = new nop();
-    return bound;
+function myNew() { 
+    // 1. 创建一个对象
+    var obj = new Object();
+    //  获取外部构造器
+    var Constructor = Array.prototype.shift.call(arguments);
+    // 2. 实现继承，绑定原型
+    obj.__proto__ = Constructor.prototype;
+    // 3. 调用构造器，并改变this指向
+    var res = Constructor.apply(obj, arguments);
+    return typeof res === 'object' ? res : obj;
 }

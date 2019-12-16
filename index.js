f01a279da2b97ad6118391e451cf16c39ff81a21
SophieU <j.u.sophia@qq.com>
2019-12-16 22:17:43 +0800

    1216

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-/**
- * React组件生命周期：
- * mounting阶段：
- * constractor: 初始化state
- * componentWillMount
- * render
- * componentDidMount
- * update阶段：
- * componentWillRecieveProps
- * shouldComponentUpdate
- * componentWillUpdate
- * componentDidUpdate
- * Unmounting阶段
- * componentWillUnmount
- * 
- */
+function instanceOf(left, right) {
+    let prototype = right.prototype;
+    let = left.__proto__
+    while (true) {
+        if (left === null) {
+            return false;
+        }
+        if (left === prototype) {
+            return true;
+        }
+        let = left.__proto__
+    }
+}

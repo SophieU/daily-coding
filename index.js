c79c82d0ca3b57637b63192444a9bdcc4714ff8e
SophieU <j.u.sophia@qq.com>
2019-12-17 20:13:47 +0800

    test111

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function instanceOf(left, right) {
-    let prototype = right.prototype;
-    let = left.__proto__
-    while (true) {
-        if (left === null) {
-            return false;
-        }
-        if (left === prototype) {
-            return true;
-        }
-        let = left.__proto__
-    }
-}
+/* */

a90137e7ffdef33edb4b0eb8afc1f1f24ce09b2a
WIN-9M659CUL90U\Administrator <j.u.sophia@qq.com>
2020-01-02 16:04:45 +0800

    20200102

--- JavaScript数据结构与算法学习一：栈.md
+++ JavaScript数据结构与算法学习一：栈.md
@@ -1,2 +1,3 @@
 ## 什么是栈？
-> 栈（Skipper）
+> 栈（Skipper）:一种后进先出（LIFO）的数据结构。栈结构的基本概念：栈顶，栈底，入栈，出栈
+![栈结构图解](https://upload-images.jianshu.io/upload_images/1102036-9253975a5d43fdb0?imageMogr2/auto-orient/strip|imageView2/2/w/614/format/webp)
--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-var arr = [3, 4, 15,80,7, 2, 6, 5, 8, 9, 10, 16, 13]; // length = 13
-       
-function bubbleSort(arr) {
-    for (let i = 0; i < arr.length - 1; i++){
-        for (let j = 0; j < arr.length - 1 - i; j++){
-            if (arr[j] > arr[j + 1]) {
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+var LinkedList = function(){
+    // 链表头
+    var head = null
+    // 链表长度 
+    var length = 0
+    // 辅助类
+    var Node = function(elem){
+        this.elem = elem
+        this.next = null
     }
-    return arr;
+    // 添加元素
+    this.append = function(el){
+        var node = new Node(el)
+        if(head===null){
+            head = node 
+        }else{
+            var current= head;
+            while(current.next){
+                current = current.next
+            }
+            current.next=node
+        }
+        length ++;
+    }
+    this.printHead = function(){
+        return head
+    }
 }
-var res = bubbleSort(arr)
-console.log(res)
+
+var l = new LinkedList()
+l.append(1)
+l.append(2)
+console.log(l.printHead())

e796c2df771b8a2938d90e53eae81ee3d3240399
SophieU <j.u.sophia@qq.com>
2020-01-14 13:43:00 +0800

    20200114

---
+++ sort.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var moveZeroes = function(nums) {
+    for (let i = 0; i < nums.length; i++){
+        
+    }
+};
+var nums = [0, 1, 0, 3, 12]
+moveZeroes(nums)

efeb11c21bb289b905408cc8084b7707350fbf1f
WIN-9M659CUL90U\Administrator <j.u.sophia@qq.com>
2020-01-20 16:00:28 +0800

    20200120

--- tree.js
+++ tree.js
@@ -4,5 +4,21 @@
  * 引用类型: object
  * 六个为false的值：
  * false,0,'',undefined,null,NaN
+ * 
+ * 类型转换：valueOf->toString->toPrimitive
  */
+
+ let a = {
+   valueOf(){
+     return 0
+   },
+   toString(){
+     return '1'
+   },
+   [Symbol.toPrimitive](){
+     return 2;
+   }
+ }
+ console.log(1+a)
+ console.log('1'+a)
 

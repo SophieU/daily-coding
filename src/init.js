f44adefa2922d2827c9b97c18d9cb5447f936bbb
SophieU <j.u.sophia@qq.com>
2021-05-08 10:39:03 +0800

    reset

--- src/init.js
+++
@@ -1,6 +0,0 @@
-const download = require('download-git-repo')
-const tmpAdd = require('../template.json').admin
-console.log(222)
-download(`direct:${tmpAdd}`, 'my-app',{clone: true}, err=>{
-    console.log(err)
-})
